4c41df7574102dea64c93567c8f5df831deaeaf9
Corey Crumpton <31524125+cecrumpton@users.noreply.github.com>
2021-05-07 22:02:53 -0500

    completed project

---
+++ Jewels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5Pg266_JewelsVirtualOverride
+{
+    class Jewels
+    {
+        public string Sparkle()
+        {
+            return "Sparkle, sparkle!";
+        }
+    }
+}
